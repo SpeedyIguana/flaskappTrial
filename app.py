b09006edf5f774770de3262cfd34c979bdf70bbf
akassh <akassh.shah77@gmail.com>
2020-05-09 19:32:38 -0400

    slow start

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return Markup("<h1>hi</h1>")
+
+if __name__ == "__main__":
+    app.run(debug = True)
